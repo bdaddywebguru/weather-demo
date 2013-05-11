8e397b1e26abca4058f7e9b8763428462ffb6e31
BDaddy WebGuru <bdaddy@whitelabelwebdeveloper.com>
2013-05-11 17:39:51 -0400

    Demo

---
+++ src/forge/app_config.js
@@ -0,0 +1,63 @@
+window.forge = {}; window.forge.config = {
+    "author": "bdaddy@whitelabelwebdeveloper.", 
+    "config_hash": "b433ecc3e0a424f0aad3cc592dcb6385160e68de", 
+    "config_version": "4", 
+    "core": {
+        "android": {
+            "package_name": "io.trigger.forgec4dad152b75f11e2888912313d00dc45"
+        }, 
+        "firefox": {
+            "package_name": "c4dad152b75f11e2888912313d00dc45"
+        }, 
+        "general": {
+            "reload": true
+        }, 
+        "ie": {
+            "package_name": "{354471C7-C3A7-20C4-91A6-A290C98359D2}"
+        }, 
+        "ios": {
+            "package_name": "io.trigger.forgec4dad152b75f11e2888912313d00dc45"
+        }, 
+        "osx": {
+            "package_name": "io.trigger.forgec4dad152b75f11e2888912313d00dc45"
+        }, 
+        "safari": {
+            "package_name": "forge.safari.weatherdemoc4dad152b75f11e2888912313d00dc45"
+        }
+    }, 
+    "description": "An empty app created by default", 
+    "json_safe_name": "Weather-Demo", 
+    "logging": {
+        "level": "DEBUG"
+    }, 
+    "name": "Weather-Demo", 
+    "package_name": "weatherdemoc4dad152b75f11e2888912313d00dc45", 
+    "platform_version": "v1.4.43", 
+    "plugins": {
+        "contact": {
+            "hash": "notahash"
+        }, 
+        "file": {
+            "hash": "notahash"
+        }, 
+        "media": {
+            "hash": "notahash"
+        }, 
+        "prefs": {
+            "hash": "notahash"
+        }, 
+        "request": {
+            "config": {
+                "permissions": [
+                    "http://*/*", 
+                    "https://*/*"
+                ]
+            }, 
+            "hash": "notahash"
+        }
+    }, 
+    "trigger_domain": "https://trigger.io", 
+    "uuid": "c4dad152b75f11e2888912313d00dc45", 
+    "version": "0.1", 
+    "xml_safe_name": "Weather-Demo"
+};
